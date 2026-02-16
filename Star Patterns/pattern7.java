3aa6aaba63eceebea9269c303951d997ea6be15f
Abhijeet Tiwari <abhijeett709@gmail.com>
2026-02-16 14:56:05 +0530

    Star pattern added with file new file name pattern 7 and patter 8

---
+++ Star Patterns/pattern7.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class pattern7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows: ");
+        int rows = sc.nextInt();
+        for(int i = rows; i>=1 ; i--)
+        {
+            for(int j = 1; j<=i ; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i = 2 ; i<=rows; i++)
+        {
+            for(int j = 1 ; j<=i ; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+    
+}
