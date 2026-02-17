a4afb4da241a4f99e97e69b4220d084877968dea
Abhijeet Tiwari <abhijeett709@gmail.com>
2026-02-17 16:37:52 +0530

    Pattern files added in Number Pattern

---
+++ Number Patterns/pattern9.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class pattern9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows");
+        int rows = sc.nextInt();
+        int copy = rows;
+        for(int i = rows ; i>=1 ; i--)
+        {
+            for(int space = 1 ; space<=copy-i ; space++)
+            {
+                System.out.print(" ");
+            }
+            for(int j = 1 ; j<=i ; j++)
+            {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+        for(int i= 2 ; i<=rows; i++)
+        {
+            for(int space = 1 ; space<=copy-i ; space++)
+            {
+                System.out.print(" ");
+            }
+            for(int j = 1 ; j<=i ; j++)
+            {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+    
+}
