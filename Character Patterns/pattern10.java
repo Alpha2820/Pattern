c5daa62cc2345ba91846178db5ea306e1ed6554b
Abhijeet Tiwari <abhijeett709@gmail.com>
2026-02-18 15:35:23 +0530

    Another character pattern files added to character Pattern folder

---
+++ Character Patterns/pattern10.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class pattern10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of Rows");
+        int rows = sc.nextInt();
+        int copy = rows;
+        for(int i = 1 ; i<=rows ; i++)
+        {
+            char ch = 'A';
+            for(int space = 1 ; space<=copy-i ; space++)
+            {
+                System.out.print(" ");
+            }
+            for(int j = 1 ; j<=i ; j++)
+            {
+                System.out.print(ch+" ");
+                ch++;
+            }
+            System.out.println();
+        }
+        for(int i = 6 ; i>=1 ;  i--)
+        {
+            char ch = 'A';
+            for(int space = 1 ; space<=copy-i ; space++)
+            {
+                System.out.print(" ");
+            }
+            for(int j = 1 ; j<=i ; j++)
+            {
+                System.out.print(ch+" ");
+                ch++;
+            }
+            System.out.println();
+        }
+        sc.close();
+
+    }
+    
+}
