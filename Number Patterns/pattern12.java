68fc44f79a5e9a13ca1cfcf6291f3421f4582e07
Abhijeet Tiwari <abhijeett709@gmail.com>
2026-02-19 16:32:55 +0530

    Completed all the number pattern and added into the number pattern folder

---
+++ Number Patterns/pattern12.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class pattern12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows");
+        int rows = sc.nextInt();
+        for(int i = 1; i<=rows ; i++)
+        {
+            for(int space = 1 ; space<=rows-i ; space++)
+            {
+                System.out.print(" ");
+            }
+            for(int j = 1 ; j<=i ; j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+        int copy = rows-1;
+        for(int i = copy ; i>=1 ; i--)
+        {
+            for(int space = 1 ; space<=rows-i ; space++)
+            {
+                System.out.print(" ");
+            }
+            for(int j = 1 ; j<=i ; j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+    
+}
